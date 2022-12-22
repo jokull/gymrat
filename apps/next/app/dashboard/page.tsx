8f75f643d51deb49e3c1188c96a02e38ce4f4d4a
Jökull Sólberg <jokull@solberg.is>
2022-12-22 19:44:27 +0000

    Lets try this

--- apps/next/app/dashboard/page.tsx
+++ apps/next/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import { type Workout } from "@gymrat/api";
+
 import { trpc } from "@/trpc/server";
 
 import { DataCreateWorkout } from "./components/CreateWorkout";
@@ -6,10 +8,11 @@ import { DataWorkouts } from "./components/Workouts";
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
-  const [workouts, user] = await Promise.all([
-    trpc.workouts.query(),
+  const [user] = await Promise.all([
+    // trpc.workouts.query(),
     trpc.user.query(),
   ]);
+  const workouts: Workout[] = [];
   return (
     <div className="flex flex-col gap-4 h-full">
       <div className="grow flex flex-col gap-4">

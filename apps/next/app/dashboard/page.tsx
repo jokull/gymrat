79aee2bccd2fe70cc44f2e4aae9d0adef63fd1f5
Jökull Sólberg <jokull@solberg.is>
2022-12-22 19:40:17 +0000

    Lets try this

--- apps/next/app/dashboard/page.tsx
+++ apps/next/app/dashboard/page.tsx
@@ -1,24 +1,23 @@
-// import { headers } from "next/headers";
-
-// import { trpc } from "@/trpc/server";
+import { trpc } from "@/trpc/server";
 
 import { DataCreateWorkout } from "./components/CreateWorkout";
 import { DataWorkouts } from "./components/Workouts";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
-  // headers(); // next.js now makes this a dynamic route
-  // const [workouts, user] = await Promise.all([
-  //   trpc.workouts.query(),
-  //   trpc.user.query(),
-  // ]);
+  const [workouts, user] = await Promise.all([
+    trpc.workouts.query(),
+    trpc.user.query(),
+  ]);
   return (
     <div className="flex flex-col gap-4 h-full">
       <div className="grow flex flex-col gap-4">
-        <DataCreateWorkout workouts={[]} data-superjson />
-        <DataWorkouts initialData={[]} data-superjson />
+        <DataCreateWorkout workouts={workouts} data-superjson />
+        <DataWorkouts initialData={workouts} data-superjson />
       </div>
       <footer>
-        {/* <p className="text-xs text-neutral-600 text-center">{user.apiKey}</p> */}
+        <p className="text-xs text-neutral-600 text-center">{user.apiKey}</p>
       </footer>
     </div>
   );

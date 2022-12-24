46b63ada886976ee19a455673228bfe81ae04d14
Jökull Sólberg <jokull@solberg.is>
2022-12-24 18:15:10 +0000

    Some trpc routing stuff

--- apps/next/trpc/server.ts
+++ apps/next/trpc/server.ts
@@ -10,8 +10,8 @@ export const trpc = createTRPCProxyClient<AppRouter>({
   links: [
     loggerLink({ enabled: () => process.env.NODE_ENV === "development" }),
     httpLink({
-      url: `https://${
-        process.env.VERCEL_URL ?? process.env.HOST ?? "www.gymrat.is"
+      url: `${process.env.NODE_ENV === "development" ? "http" : "https"}://${
+        process.env.TRPC_HOST ?? ""
       }/trpc`,
       fetch(url, options) {
         return fetch(url, { ...options, credentials: "include" });
